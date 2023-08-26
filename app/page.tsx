fc209668eac89261c2e83e00f0faa66496b6320c
teebloc <102591864+teebloc@users.noreply.github.com>
2023-08-26 15:17:22 +0800

    Created a basic home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
+"use client"
+import { useState, useEffect, useRef } from 'react';
+
 export default function Home() {
-  return <h1>Home</h1>;
+  const [Login_Username, set_Login_Username] = useState(localStorage.getItem('Login_Username')) //Get the Login Username. will be null if not logged in
+  if (!Login_Username) { //if Login_Username is null, means user is not logged in
+    //reroute to login page
+  } else {
+    return (
+      <h1>
+          Hello {localStorage.getItem('Login_Username')}!
+      </h1>
+    );
+  }
 }
