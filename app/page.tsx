65e16cd1dd667f459f9c60e7b194f5556d458aed
teebloc <102591864+teebloc@users.noreply.github.com>
2023-08-26 15:21:48 +0800

    Created a basic Login Page without functionalities

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,8 @@ export default function Home() {
   const [Login_Username, set_Login_Username] = useState(localStorage.getItem('Login_Username')) //Get the Login Username. will be null if not logged in
   if (!Login_Username) { //if Login_Username is null, means user is not logged in
     //reroute to login page
+    console.log('hi')
+    window.location.href = '/Login';
   } else {
     return (
       <h1>
