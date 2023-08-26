8344d06dc24472b9ab6f05a4398e2c1ac99647b7
Ike <ikelimjl@gmail.com>
2023-08-26 15:20:19 +0800

    supabase set up

---
+++ supabase/database.js
@@ -0,0 +1,27 @@
+import supabase from "./config";
+import { handlePassword } from "./helpers";
+
+class userDB {
+  async addUser(username, password) {
+    const hashed = handlePassword(password);
+    const { data, error } = await supabase
+      .from("users")
+      .insert([{ username: username, password: hashed }])
+      .select();
+
+    return { data, error };
+  }
+
+  async loginUser(username, password) {
+    const hashed = handlePassword(password);
+
+    let { data: users, error } = await supabase
+      .from("users")
+      .select("password")
+      .eq("username", username);
+
+    return { data, error };
+  }
+}
+
+export const userDatabase = new userDB();
---
+++ supabase/helpers.js
@@ -0,0 +1,8 @@
+import bcrypt from "bcryptjs";
+
+export function handlePassword(password) {
+  const salt = bcrypt.genSaltSync(10);
+  const hashed = bcrypt.hashSync(password, salt);
+
+  return hashed;
+}
