82dd7ca6e8e03b106bfb1c6e3a0e5d1658936ba7
teebloc <102591864+teebloc@users.noreply.github.com>
2023-08-26 15:43:46 +0800

    Added Function to Login Page and made a working Logout button in home page

--- app/Login/page.tsx
+++ app/Login/page.tsx
@@ -1,7 +1,38 @@
+'use client'
+
 export default function Page() {
+  function OnLogin(event: any) {
+    event.preventDefault();
+
+    const Username = event.target[0].value
+    const Password = event.target[1].value
+    const is_Verified = Verify_User(Username, Password)
+    if (is_Verified) {
+      localStorage.setItem('Login_Username', Username)
+      window.location.href = '/';
+    } else if (!is_Verified) {
+      console.log('wrong login details!')
+      //will create proper error messages later in the future
+    }
+  }
+
+  function Verify_User(Username: string, Password: string) {
+    //API call to backend to verify user's login details
+    //returns true always for now since backend isnt developed yet
+
+    return true
+  }
+
   return (
     <div>
       <h1>Login Page</h1>
+
+      <form onSubmit={OnLogin}>
+        <input type='text' /> <br />
+        <input type='text' /> <br />
+
+        <input type='submit' />
+      </form>
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,22 @@ import { useState, useEffect, useRef } from 'react';
 
 export default function Home() {
   const [Login_Username, set_Login_Username] = useState(localStorage.getItem('Login_Username')) //Get the Login Username. will be null if not logged in
+  
+  function Logout (event: any) {
+    localStorage.removeItem('Login_Username') //remove Login_Username from localstorage so client is no longer logged in
+    window.location.reload(); //reload the page
+  }
   if (!Login_Username) { //if Login_Username is null, means user is not logged in
     //reroute to login page
-    console.log('hi')
     window.location.href = '/Login';
   } else {
+
     return (
-      <h1>
-          Hello {localStorage.getItem('Login_Username')}!
-      </h1>
+      <div>
+        <h1>Hello {localStorage.getItem('Login_Username')}!</h1> <br />
+
+        <button onClick={Logout}>Logout</button>
+      </div>
     );
   }
 }
