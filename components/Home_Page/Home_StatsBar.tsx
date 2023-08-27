e8cd5ee5c8d86d8d394d5ffa657d025ee6ccb21f
teebloc <102591864+teebloc@users.noreply.github.com>
2023-08-27 09:36:56 +0800

    Made the Add payment button in home page to link to /create link

--- components/Home_Page/Home_StatsBar.tsx
+++ components/Home_Page/Home_StatsBar.tsx
@@ -22,7 +22,7 @@ export default function StatsBar(props: any) {
 
   const services_list = [
     { name: "Connect bank", link: "", icon: "bank" },
-    { name: "Add payment", link: "", icon: "add-card" },
+    { name: "Add payment", link: "/create", icon: "add-card" },
     { name: "Top up manually", link: "", icon: "add-wallet" },
   ];
 
