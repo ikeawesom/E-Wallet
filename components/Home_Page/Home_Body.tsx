f086166a1ca15bdcc497a9ca815dd5ce49875a11
Ike <ikelimjl@gmail.com>
2023-08-27 11:15:07 +0800

    fixed bugs

--- components/Home_Page/Home_Body.tsx
+++ components/Home_Page/Home_Body.tsx
@@ -7,30 +7,29 @@ export default function HomeBody(props: any) {
   const paymentData = props.data.payments;
   const newUser = props.newUser;
 
-  if (paymentData)
-    return (
-      <div className="flex flex-col gap-8 bg-light-sec-color p-10 rounded-lg w-full md:w-3/4 md:h-[80vh]">
-        <h1 className="text-3xl">
-          Welcome back,{" "}
-          <span className="font-bold text-sec-color">{username}</span>! What
-          shall we do today?
-        </h1>
+  return (
+    <div className="flex flex-col gap-8 bg-light-sec-color p-10 rounded-lg w-full md:w-3/4 md:h-[80vh]">
+      <h1 className="text-3xl">
+        Welcome back,{" "}
+        <span className="font-bold text-sec-color">{username}</span>! What shall
+        we do today?
+      </h1>
 
-        {newUser && (
-          <div className="rounded-lg bg-gradient-to-br from-primary-color to-sec-color p-8 flex justify-between items-center gap-10">
-            <div>
-              <h1 className="text-2xl text-grey font-light">
-                <span className="font-bold">Fully automated</span> payment
-                system designed just for you
-              </h1>
-            </div>
-            <button className="px-4 py-2 border-2 border-white rounded-lg text-white font-bold text-lg hover:bg-white hover:text-sec-color duration-200">
-              Get started
-            </button>
+      {newUser && (
+        <div className="rounded-lg bg-gradient-to-br from-primary-color to-sec-color p-8 flex justify-between items-center gap-10">
+          <div>
+            <h1 className="text-2xl text-grey font-light">
+              <span className="font-bold">Fully automated</span> payment system
+              designed just for you
+            </h1>
           </div>
-        )}
+          <button className="px-4 py-2 border-2 border-white rounded-lg text-white font-bold text-lg hover:bg-white hover:text-sec-color duration-200">
+            Get started
+          </button>
+        </div>
+      )}
 
-        <Home_List_of_Subs data={paymentData} />
-      </div>
-    );
+      <Home_List_of_Subs data={paymentData} />
+    </div>
+  );
 }
